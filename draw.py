1e0d1193bcac3b795b88bb8fb831581c4d6d4328
Xing Tao Shi <xshi@stuy.edu>
2018-02-09 08:46:33 -0500

    fin

--- draw.py
+++ draw.py
@@ -1,4 +1,78 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+
+    if (x0 > x1):
+        temp = x1
+        x1 = x0
+        x0 = temp
+        temp = y1
+        y1 = y0
+        y0 = temp
+
+    A = y1 - y0
+    B = -1 * (x1 - x0)
+    X = x0
+    Y = y0
+
+    #undefined slope
+    if (B == 0):
+        if (y0 < y1):
+            while (Y <= y1) :
+                plot(screen, color, X, Y)
+                Y += 1
+        else:
+            while (Y >= y1) :
+                plot(screen, color, X, Y)
+                Y -= 1
+        return
+
+    slope = A * 1.0 / -B
+
+    #OCTANT 1
+    if (slope <= 1 and slope >= 0):
+        d = 2 * A + B        
+        while (X <= x1) :
+            plot(screen, color, X, Y)
+            if d > 0:
+                Y += 1
+                d += (2 * B)
+            X += 1
+            d += (2* A)
+    #OCTANT 8
+    elif (slope >= -1 and slope < 0):
+        d = (2 * A) - B
+        while (X <= x1) :
+            plot(screen, color, X, Y)
+            if d < 0:
+                Y -= 1
+                d -= (2 * B)
+            X += 1
+            d += (2 * A)
+
+    #OCTANT 2
+    elif (slope > 1):
+        d = A + (B * 2)
+        while (Y <= y1) :
+            plot(screen, color, X, Y)
+            if d < 0:
+                X += 1
+                d += (2 * A)
+            Y += 1
+            d += (2 * B)
+    #OCTANT 7
+    elif (slope < -1):
+        d =  A - (2 * B)
+        while (Y >= y1) :
+            plot(screen, color, X, Y)
+            if d > 0:
+                X += 1
+                d += (2 * A)
+            Y -= 1
+            d -= (2 * B)
+    else:
+        print "bro"
+
+                
+'''
+'''
--- main.py
+++ main.py
@@ -4,5 +4,38 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
+
+#--------------TESTING-------------------
+
+draw_line(250, 250, 500, 400, screen, color) #OCT 1
+draw_line(250, 250, 500, 500, screen, [0,0,255]) #OCT 1-2
+draw_line(250, 250, 400, 500, screen, [255,0,0]) #OCT 2
+draw_line(250, 250, 250, 500, screen, color) #OCT 2-3 UNDEF
+draw_line(250, 250, 100, 500, screen, [0,0,255]) #OCT 3
+draw_line(250, 250, 0, 500, screen, [255,0,0]) #OCT 3-4
+draw_line(250, 250, 0, 400, screen, color) #OCT 4
+draw_line(250, 250, 0, 250, screen, [0,0,255]) #OCT 4-5
+draw_line(250, 250, 0, 100, screen, [255,0,0]) #OCT 5
+draw_line(250, 250, 0, 0, screen, color) #OCT 5-6
+draw_line(250, 250, 100, 0, screen, [0,0,255]) #OCT 6
+
+draw_line(250, 250, 250, 0, screen, [255,0,0]) #OCT 6-7 UNDEF
+
+draw_line(250, 250, 400, 0, screen, color) #OCT 7
+draw_line(250, 250, 500, 0, screen, [0,0,255]) #OCT 7-8
+draw_line(250, 250, 500, 100, screen, [255,0,0]) #OCT 8
+draw_line(250, 250, 500, 250, screen, [255,255,255]) #OCT 8-1
+
+
+
+#draw_line(0, 400, 300, 300, screen, color)
+#draw_line(0, 400, 200, 100, screen, [0,0,255])
+
+#draw_line(400, 400, 200, 200, screen, [0,0,255])
+#--------------TESTING-------------------
+
+
 display(screen)
 save_extension(screen, 'img.png')
+
+
